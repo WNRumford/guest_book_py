186ae08f4ddba733bcc280ab3ea8b86361e367a6
Kirill <dukennt@gmail.com>
2018-02-10 20:27:08 +0000

    ended method 'read_msgs', and __init__()

--- _gbook.py
+++ _gbook.py
@@ -1,3 +1,6 @@
+import json
+from datetime import datetime
+#1.37
 class Gbook:
     LOGIN = "admin"
     PASSWORD = "pass@word1"
@@ -13,8 +16,35 @@ class Gbook:
         self.content = ""
         self.logform = ""
         
+        with open(self.TMP_FORM, 'r', encoding='utf-8') as f:
+            self.form = f.read()
+            
+        with open(self.TMP_CONTENT, 'r', encoding='utf-8') as f:
+            self.content = f.read()
+        
+        try:
+            with open(self.DB, 'r', encoding='utf-8') as f:
+                pass
+        except FileNotFoundError:
+            with open(self.DB, 'w', encoding='utf-8') as f:
+                json.dump({}, f)
+            
+        
     def read_msgs(self):
-        pass
+        with open(self.DB, 'r', encoding='utf-8') as f:
+            m = json.load(f)
+        if not m:
+            self.messages = "There are no any notes!"
+            return
+        with open(self.TMP_MESSAGE, 'r', encoding='utf-8') as f:
+            pattern = f.read()
+        for id in sorted(m.keys(), reverse=True):
+            date = datetime.fromtimestamp(int(id)).strftime('%d-%m-%Y %H:%M:%S')
+            self.messages += pattern.format(
+                                            msg=m[id]["msg"],
+                                            datetime=date,
+                                            name=m[id]["name"],
+                                            email=m[id]["email"])
     
     def save_msg(self, name, email, msg):
         pass
--- index.py
+++ index.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-#1.17
+
 from _gbook import Gbook
 
 print("Content-Type: text/html;charset=utf-8")
 print()
+
 gbook = Gbook()
 print(gbook.content.format(logform=gbook.logform, 
                           form=gbook.form,
