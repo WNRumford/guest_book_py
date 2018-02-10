b70843677e309e7869ed0620c71691136043f393
Kirill <dukennt@gmail.com>
2018-02-10 18:07:43 +0000

    Начал заново. Повторю. Создал индекс и класс gbook

--- index.py
+++ index.py
@@ -1,26 +1,10 @@
 #!/usr/bin/env python3
+#1.17
 from _gbook import Gbook
-from cgi import FieldStorage
-import os
-import html
 
-gbook = Gbook()
-params = FieldStorage()
-
-if os.environ.get("REQUEST_METHOD") == "POST":
-    operation = params.getvalue("operation", "")
-    if operation == "post":
-        name = html.escape(params.getvalue("name"))
-        email = html.escape(params.getvalue("email"))
-        msg = html.escape(params.getvalue("msg"))
-        gbook.save_msg(name, email, msg)
-
-print('Conten-Type: text/html; charset=utf-8')
+print("Content-Type: text/html;charset=utf-8")
 print()
-
-gbook.read_msgs()
-print(gbook.content.format(
-                           logform=gbook.logform,
-                           form=gbook.form,
-                           messages=gbook.messages
-                           ))
+gbook = Gbook()
+print(gbook.content.format(logform=gbook.logform, 
+                          form=gbook.form,
+                          messages=gbook.messages))
